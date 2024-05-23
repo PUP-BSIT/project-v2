eeaf75643779baa525bb0dc2cb749bc21d2fa84b
hassan_adnan <adnanhassanda@gmail.com>
2024-05-24 00:53:30 +0800

    delete project to redo

--- huenique/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-
-export const routes: Routes = [
-    { path: '', component: LandingComponent},
-    { path: 'signup-component', component: SignupComponent}
-];
