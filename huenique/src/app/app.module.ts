fb52a73981276a7abddd6afc47db364b68592124
hassan_adnan <adnanhassanda@gmail.com>
2024-05-24 14:00:30 +0800

    add signup component

--- huenique/src/app/app.module.ts
+++ huenique/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
