17b1863d442e6cb6ee4b670cf1b480943f12ff44
hassan_adnan <adnanhassanda@gmail.com>
2024-05-31 17:23:32 +0800

    add signup backend functionality

--- huenique/src/app/signup/signup.component.ts
+++ huenique/src/app/signup/signup.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { SignupService } from '../../service/signup.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']  // Note the change here from styleUrl to styleUrls
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private signupService: SignupService) {}
+
+  get nameControl() {
+    return this.signupForm.get('name');
+  }
+
+  get usernameControl(){
+    return this.signupForm.get('username');
+  }
+
+  get emailControl(){
+    return this.signupForm.get('email');
+  }
+
+  get passwordControl(){
+    return this.signupForm.get('password');
+  }
+
+  get confirmPasswordControl(){
+    return this.signupForm.get('confirmPassword');
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      name: ['', [Validators.required, this.noNumericCharactersValidator]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  noNumericCharactersValidator(control: AbstractControl): { [key: string]: any } | null {
+    const isValid = /^[^\d]*$/.test(control.value);
+    return isValid ? null : { noNumericCharacters: true };
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.get('password')?.value === form.get('confirmPassword')?.value ? null : { mismatch: true };
+  }
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      this.signupService.signup(this.signupForm.value).subscribe(
+        response => {
+          console.log('User created successfully', response);
+        },
+        error => {
+          console.log('Error creating user', error);
+        }
+      );
+    }
+  }
 }
--- huenique/src/service/signup.service.ts
+++ huenique/src/service/signup.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
+  private apiUrl = 'http://localhost:3000/api/signup';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signup(user: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user);
+  }
 }
