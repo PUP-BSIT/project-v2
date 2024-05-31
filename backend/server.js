104f7eb30e6c5bd7f2b65b3e42907e7c3b58e625
hassan_adnan <adnanhassanda@gmail.com>
2024-06-01 00:36:39 +0800

    add login backend

--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,44 @@ app.post('/api/signup', async (req, res) => {
   }
 });
 
+app.post('/api/login', (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  const sql = 'SELECT * FROM user WHERE email = ?';
+  connection.query(sql, [email], async (err, results) => {
+    if (err) {
+      console.error('Error fetching user: ' + err.stack);
+      return res.status(500).json({ error: 'Database error', details: err });
+    }
+
+    if (results.length === 0) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const user = results[0];
+    console.log('Stored Hashed Password:', user.password);
+
+    try {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      console.log('Is Password Valid:', isPasswordValid);
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ error: 'Invalid password' });
+      }
+
+      
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      res.status(200).json({ message: 'Login successful', token });
+    } catch (bcryptErr) {
+      console.error('Error comparing password: ' + bcryptErr.stack);
+      return res.status(500).json({ error: 'Password comparison failed' });
+    }
+  });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
