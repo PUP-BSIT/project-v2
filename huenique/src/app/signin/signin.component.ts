a84a0dd4cdc332f96c0b345ba39e62ef15aa8d54
hassan_adnan <adnanhassanda@gmail.com>
2024-06-01 00:58:47 +0800

    add validator

--- huenique/src/app/signin/signin.component.ts
+++ huenique/src/app/signin/signin.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, EmailValidator } from '@angular/forms';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit{
+  loginForm! : FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) {}
+
+  get emailControl(){
+    return this.loginForm.get('email');
+  }
+
+  get passwordControl(){
+    return this.loginForm.get('password');
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid){
+      console.log(this.loginForm.value);
+      this.loginService.login(this.loginForm.value).subscribe(
+        response => {
+          console.log('user logged in successfully', response);
+        },
+        error => {
+          console.log('error logging in', error);
+        }
+      )
+    }
+  }
 }
