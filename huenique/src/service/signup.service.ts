b8ecbf2f454b6d2be6d3390261d1f9bcacc41765
hassan_adnan <adnanhassanda@gmail.com>
2024-05-31 17:48:23 +0800

    fix to not use any

--- huenique/src/service/signup.service.ts
+++ huenique/src/service/signup.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../app/model/user';
+import { SignupResponse } from '../app/model/signupResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class SignupService {
 
   constructor(private http: HttpClient) {}
 
-  signup(user: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, user);
-  }
+  signup(user: User): Observable<SignupResponse> {
+    return this.http.post<SignupResponse>(this.apiUrl, user);
+  }  
 }
