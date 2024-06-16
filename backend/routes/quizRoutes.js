ea6742c8098cb702d8161e892eb05fdc18c1025a
hassan_adnan <adnanhassanda@gmail.com>
2024-06-16 16:27:35 +0800

    update route

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const quizController = require('../controllers/quizController');
 
-router.get('/questions', quizController.getQuestions);
+router.get('/questions', quizController.getQuestionsWithOptions);
 
 module.exports = router;
