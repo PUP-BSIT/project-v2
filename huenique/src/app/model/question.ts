e1c36cdb8ed1c1a5dd1c5adfcc82f8a6ea23fb75
hassan_adnan <adnanhassanda@gmail.com>
2024-06-16 15:57:07 +0800

    create question service

---
+++ huenique/src/app/model/question.ts
@@ -0,0 +1,5 @@
+export interface Question {
+  question_id: number;
+  question_text: string;
+  season_id: number;
+}
--- huenique/src/service/question.service.ts
+++ huenique/src/service/question.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Question } from '../app/model/question';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/questions';
+
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>(this.apiUrl);
+  }
 }
