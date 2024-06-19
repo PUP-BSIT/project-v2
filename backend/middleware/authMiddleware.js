72a5573fbc72876bdca01164493f14891097c19d
hassan_adnan <adnanhassanda@gmail.com>
2024-06-19 16:08:01 +0800

    add backend verification token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
   if (!token) {
     return res.status(403).json({ error: 'No token provided' });
   }
