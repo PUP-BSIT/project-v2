c9fb3ccddab4c12fd1358e8738035e420d0b318b
hassan_adnan <adnanhassanda@gmail.com>
2024-06-30 22:49:48 +0800

    fix bug in email request result

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,12 @@ app.use(cors());
 const authRoutes = require('./routes/authRoutes');
 const profRoutes = require('./routes/profRoutes');
 const quizRoutes = require('./routes/quizRoutes');
+const emailRoutes = require('./routes/emailRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api', profRoutes);
 app.use('/api', quizRoutes);
+app.use('/api', emailRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
