b29b456366a5ab9056adb0e26a6c6cf96caef66a
hassan_adnan <adnanhassanda@gmail.com>
2024-07-01 23:10:10 +0800

    edit server js to use clinic

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,13 @@ const authRoutes = require('./routes/authRoutes');
 const profRoutes = require('./routes/profRoutes');
 const quizRoutes = require('./routes/quizRoutes');
 const emailRoutes = require('./routes/emailRoutes');
+const clinicRoutes = require('./routes/clinicRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api', profRoutes);
 app.use('/api', quizRoutes);
 app.use('/api', emailRoutes);
+app.use('/api', clinicRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
