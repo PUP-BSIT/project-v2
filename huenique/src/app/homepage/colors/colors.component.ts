906a5474b1f05d4c5cdb554db7c0b5df1c8adabb
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-07-09 01:16:37 +0800

    modify changes

--- huenique/src/app/homepage/colors/colors.component.ts
+++ huenique/src/app/homepage/colors/colors.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-colors',
   templateUrl: './colors.component.html',
-  styleUrl: './colors.component.css'
+  styleUrls: ['./colors.component.css']
 })
 export class ColorsComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(season: string, color: string) {
+    this.router.navigate([`/homepage/colors/${season}/${color}`]);
+  }
 }
