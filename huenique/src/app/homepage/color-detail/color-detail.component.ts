2faa4f5f1660e04005d245f36285b6b982339b79
hassan_adnan <adnanhassanda@gmail.com>
2024-07-10 02:01:24 +0800

    modify color category ui

--- huenique/src/app/homepage/color-detail/color-detail.component.ts
+++ huenique/src/app/homepage/color-detail/color-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ColorService } from '../../../service/color.service';
-
 @Component({
   selector: 'app-color-detail',
   templateUrl: './color-detail.component.html',
@@ -9,6 +8,8 @@ import { ColorService } from '../../../service/color.service';
 })
 export class ColorDetailComponent implements OnInit {
   colorDetails: any;
+  filteredColorDetails: any;
+  selectedCategory: string = 'accessories';
 
   constructor(private route: ActivatedRoute, private colorService: ColorService) {}
 
@@ -16,6 +17,18 @@ export class ColorDetailComponent implements OnInit {
     const subcategoryId = this.route.snapshot.paramMap.get('subcategory_id');
     this.colorService.getColorDetails(Number(subcategoryId)).subscribe((data: any) => {
       this.colorDetails = data.data;
+      this.filterColorDetails();
     });
+  }
+
+  setCategory(category: string): void {
+    this.selectedCategory = category;
+    this.filterColorDetails();
+  }
+
+  filterColorDetails(): void {
+    this.filteredColorDetails = this.colorDetails.filter(
+      (item: any) => item.category.toLowerCase() === this.selectedCategory
+    );
   }
 }
