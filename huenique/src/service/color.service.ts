733abf76363bc1444d259c09c9a432424ab339d7
hassan_adnan <adnanhassanda@gmail.com>
2024-07-10 01:17:51 +0800

    remove comment

--- huenique/src/service/color.service.ts
+++ huenique/src/service/color.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ColorService {
-  private apiUrl = 'http://localhost:3000/api'; // Update this URL
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) {}
 
